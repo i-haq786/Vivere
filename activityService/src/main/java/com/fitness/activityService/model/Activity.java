99e16a7df57fd438bb38552214789185a15ad909
i-haq786 <i.haqcs@gmail.com>
2026-02-08 17:51:56 +0530

    add activity model

--- activityService/src/main/java/com/fitness/activityService/model/Activity.java
+++ activityService/src/main/java/com/fitness/activityService/model/Activity.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
