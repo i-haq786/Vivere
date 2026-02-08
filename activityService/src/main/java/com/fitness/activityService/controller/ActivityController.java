f99040f1dacd828bd9a49bb78f2e3dc99d3bae8d
i-haq786 <i.haqcs@gmail.com>
2026-02-09 00:04:19 +0530

    add filter activity

--- activityService/src/main/java/com/fitness/activityService/controller/ActivityController.java
+++ activityService/src/main/java/com/fitness/activityService/controller/ActivityController.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/activities")
 @AllArgsConstructor
@@ -18,4 +20,18 @@ public class ActivityController {
     public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request){
         return ResponseEntity.ok(activityService.trackActivity(request));
     }
+
+        //to return the activity of a specific user
+        @GetMapping
+        public ResponseEntity<List<ActivityResponse>> getUserActivities(@RequestHeader("X-User-ID") String userId){
+            return ResponseEntity.ok(activityService.getUserActivities(userId));
+        }
+
+        //to return activity by id
+        @GetMapping("/{activityId}")
+        public ResponseEntity<ActivityResponse> getActivityById(@PathVariable String activityId)
+        {
+            return ResponseEntity.ok(activityService.getActivityById(activityId));
+        }
+
 }
