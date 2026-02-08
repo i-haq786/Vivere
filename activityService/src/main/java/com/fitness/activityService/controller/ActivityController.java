8a63c4c049a441862da3c2f6c590ac7439ead018
i-haq786 <i-haqcs@gmail.com>
2026-02-08 17:13:04 +0530

    Initial commit

---
+++ activityService/src/main/java/com/fitness/activityService/controller/ActivityController.java
@@ -0,0 +1,21 @@
+package com.fitness.activityService.controller;
+
+import com.fitness.activityService.dto.ActivityRequest;
+import com.fitness.activityService.dto.ActivityResponse;
+import com.fitness.activityService.service.ActivityService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/activities")
+@AllArgsConstructor
+public class ActivityController {
+
+    private ActivityService activityService;
+
+    @PostMapping
+    public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request){
+        return ResponseEntity.ok(activityService.trackActivity(request));
+    }
+}
---
+++ activityService/src/main/java/com/fitness/activityService/service/ActivityService.java
@@ -0,0 +1,45 @@
+package com.fitness.activityService.service;
+
+import com.fitness.activityService.dto.ActivityRequest;
+import com.fitness.activityService.dto.ActivityResponse;
+import com.fitness.activityService.model.Activity;
+import com.fitness.activityService.repository.ActivityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor // this only generates constructor for final and not null fields
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+        Activity activity=Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .caloriesBurned(request.getCaloriesBurned())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+
+        Activity savedActivity = activityRepository.save(activity);
+
+        return mapToResponse(savedActivity);
+    }
+
+    private ActivityResponse mapToResponse (Activity activity){
+        ActivityResponse activityResponse= new ActivityResponse();
+        activityResponse.setId(activity.getId());
+        activityResponse.setUserId(activity.getUserId());
+        activityResponse.setType(activity.getType());
+        activityResponse.setDuration(activity.getDuration());
+        activityResponse.setCaloriesBurned(activity.getCaloriesBurned());
+        activityResponse.setStartTime(activity.getStartTime());
+        activityResponse.setAdditionalMetrics(activity.getAdditionalMetrics());
+        activityResponse.setCreatedAt(activity.getCreatedAt());
+        activityResponse.setUpdatedAt(activity.getUpdatedAt());
+
+        return activityResponse;
+    }
+}
