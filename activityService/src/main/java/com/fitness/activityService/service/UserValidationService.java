950e2e880c974b2ac3a2eea9e3eb6492a2c97c22
i-haq786 <i.haqcs@gmail.com>
2026-02-09 19:27:23 +0530

    success: Interservice communication

---
+++ activityService/src/main/java/com/fitness/activityService/service/UserValidationService.java
@@ -0,0 +1,28 @@
+package com.fitness.activityService.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+@Service
+@AllArgsConstructor
+public class UserValidationService {
+
+    private final WebClient userServiceWebClient;
+
+    public boolean validateUser(String userId) {
+        try {
+            return Boolean.TRUE.equals(userServiceWebClient.get()
+                    .uri("/api/users/validateUser/{userId}", userId)
+                    .retrieve()
+                    .bodyToMono(Boolean.class)
+                    .block());
+        } catch (WebClientResponseException e) {
+            if (e.getStatusCode() == HttpStatus.BAD_REQUEST)
+                throw new RuntimeException("Check your DB connection. Entered UserId that's not found is:" + userId);
+        }
+        return false;
+    }
+}
