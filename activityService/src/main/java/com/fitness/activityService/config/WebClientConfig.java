0d454ede3ca494d908f12e9e8274dc9c62cda88f
i-haq786 <i-haqcs@gmail.com>
2026-02-09 18:01:56 +0530

    add webClientConfig.java

---
+++ activityService/src/main/java/com/fitness/activityService/config/WebClientConfig.java
@@ -0,0 +1,23 @@
+package com.fitness.activityService.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    @LoadBalanced
+    //allows webclient to resolve service via eureka: which ever ip the services are u can call it by its name.
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient userServiceWebClient(WebClient.Builder webClientBuilder) {
+        return webClientBuilder
+                .baseUrl("http://USER-SERVICE")
+                .build();
+    }
+}
