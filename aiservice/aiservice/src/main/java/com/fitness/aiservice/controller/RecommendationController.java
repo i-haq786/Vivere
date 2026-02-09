d9f97dda5f12bba213b3f769176759d31231bdf7
i-haq786 <i-haqcs@gmail.com>
2026-02-10 00:16:19 +0530

    add recommendations aiservice

--- aiservice/aiservice/src/main/java/com/fitness/aiservice/controller/RecommendationController.java
+++ aiservice/aiservice/src/main/java/com/fitness/aiservice/controller/RecommendationController.java
@@ -1,19 +1,17 @@
 package com.fitness.aiservice.controller;
 
+import com.fitness.aiservice.dto.RecommendationRequest;
 import com.fitness.aiservice.dto.RecommendationResponse;
 import com.fitness.aiservice.service.RecommendationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/ai-service")
+@RequestMapping("/api/recommendation")
 public class RecommendationController {
     private final RecommendationService recommendationService;
 
@@ -22,4 +20,8 @@ public class RecommendationController {
         return ResponseEntity.ok(recommendationService.getUserRecommendations(userId));
     }
 
+    @PostMapping
+    public ResponseEntity<RecommendationResponse> addRecommendations(@RequestBody RecommendationRequest recommendationRequest) {
+        return ResponseEntity.ok(recommendationService.addRecommendation(recommendationRequest));
+    }
 }
