22e9e00581739448f5c662f5e16d429f4c579ab6
i-haq786 <i-haqcs@gmail.com>
2026-02-09 23:24:21 +0530

    add getUser recommendations

--- aiservice/aiservice/src/main/java/com/fitness/aiservice/controller/RecommendationController.java
+++ aiservice/aiservice/src/main/java/com/fitness/aiservice/controller/RecommendationController.java
@@ -1,12 +1,25 @@
 package com.fitness.aiservice.controller;
 
+import com.fitness.aiservice.dto.RecommendationResponse;
 import com.fitness.aiservice.service.RecommendationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/ai-service")
 public class RecommendationController {
     private final RecommendationService recommendationService;
+
+    @GetMapping
+    public ResponseEntity<List<RecommendationResponse>> getUserRecommendation(@PathVariable String userId) {
+        return ResponseEntity.ok(recommendationService.getUserRecommendations(userId));
+    }
 
 }
