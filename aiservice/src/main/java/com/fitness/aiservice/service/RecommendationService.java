ba08c8081d22d970615350dc9e2ca597b44498c5
i-haq786 <i-haqcs@gmail.com>
2026-02-20 07:41:13 +0530

    mongo db test folder issue fixed, other setup for mac done

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/RecommendationService.java
@@ -0,0 +1,64 @@
+package com.fitness.aiservice.service;
+
+import com.fitness.aiservice.dto.RecommendationRequest;
+import com.fitness.aiservice.dto.RecommendationResponse;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationService {
+    private final RecommendationRepository recommendationRepository;
+
+    private RecommendationResponse mapToResponse(Recommendation recommendation) {
+        RecommendationResponse recommendationResponse = new RecommendationResponse();
+        recommendationResponse.setId(recommendation.getId());
+        recommendationResponse.setActivityId(recommendation.getActivityId());
+        recommendationResponse.setUserId(recommendation.getUserId());
+        recommendationResponse.setActivityType(recommendation.getActivityType());
+        recommendationResponse.setRecommendation(recommendation.getRecommendation());
+        recommendationResponse.setImprovements(recommendation.getImprovements());
+        recommendationResponse.setSuggestions(recommendation.getSuggestions());
+        recommendationResponse.setSafety(recommendation.getSafety());
+        recommendationResponse.setCreatedAt(recommendation.getCreatedAt());
+
+        return recommendationResponse;
+    }
+
+    public List<RecommendationResponse> getUserRecommendations(String userId) {
+        List<Recommendation> allRecommendations = recommendationRepository.findByUserId(userId);
+
+        if (allRecommendations.isEmpty())
+            throw new RuntimeException("No recommendation found for the user id: " + userId);
+
+        return allRecommendations.stream()
+                .map(this::mapToResponse)
+                .toList();
+    }
+
+    public RecommendationResponse addRecommendation(RecommendationRequest recommendationRequest) {
+        Recommendation recommendation = Recommendation.builder()
+                .activityId(recommendationRequest.getActivityId())
+                .userId(recommendationRequest.getUserId())
+                .activityType(recommendationRequest.getActivityType())
+                .recommendation(recommendationRequest.getRecommendation())
+                .improvements(recommendationRequest.getImprovements())
+                .suggestions(recommendationRequest.getSuggestions())
+                .safety(recommendationRequest.getSafety())
+                .build();
+
+        return mapToResponse(recommendationRepository.save(recommendation));
+    }
+
+    public RecommendationResponse getActivityRecommendation(String activityId) {
+
+        return mapToResponse(recommendationRepository.findByActivityId(activityId)
+                .orElseThrow(() ->
+                        new RuntimeException("No activity exists with this activityId: " + activityId)
+                ));
+    }
+}
