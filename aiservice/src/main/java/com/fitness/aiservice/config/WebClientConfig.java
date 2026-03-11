2bebd02cf39815c28acd9f19bbc2d25f8cbdd7eb
i-haq786 <i.haqcs@gmail.com>
2026-03-11 13:57:08 +0530

    Feat: Generated AI recommendation from Gemini and logged

--- aiservice/src/main/java/com/fitness/aiservice/config/WebClientConfig.java
+++ aiservice/src/main/java/com/fitness/aiservice/config/WebClientConfig.java
@@ -1,6 +1,5 @@
 package com.fitness.aiservice.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,7 +7,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
     @Bean
-    @LoadBalanced
     //allows webclient to resolve service via eureka: whichever ip the services are u can call it by its name.
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
